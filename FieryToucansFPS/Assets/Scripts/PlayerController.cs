082a41b3811234da6018abdacf9a70d2cd81224e
Jeremy Torres <torres.jeremy1029@gmail.com>
2022-08-08 11:58:31 -0400

    Player movement and jumping behavior implemented.

---
+++ FieryToucansFPS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("----- Componets -----")]
+
+    [SerializeField] CharacterController controller;
+
+    [Header("----- Player Attributes -----")]
+
+    [SerializeField] float playerSpeed;
+    [SerializeField] float sprintMult;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravityValue;
+    [SerializeField] int maxJumps;
+
+    Vector3 playerVelocity;
+    Vector3 move = Vector3.zero;
+
+    float playerSpeedOrignal;
+    int timesJumped;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpeedOrignal = playerSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement() {
+
+        //Player is currently on the ground and is not jumping
+        if(controller.isGrounded && playerVelocity.y < 0) {
+
+            playerVelocity.y = 0.0f;
+            timesJumped = 0;
+
+        }
+
+        //Getting input from Unity Input Manager
+        move = (transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"));
+
+        //Adding the move vector to the character controller
+        controller.Move(move * playerSpeed * Time.deltaTime);
+
+        //Jumping functionallity
+        if(Input.GetButtonDown("Jump") && timesJumped < maxJumps) {
+            playerVelocity.y = jumpHeight;
+            timesJumped++;
+        }
+
+        playerVelocity.y -= gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
