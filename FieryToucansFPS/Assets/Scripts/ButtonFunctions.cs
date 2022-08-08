a3460675fc4effc6d9d43a866a3266d4d6f448ac
JordanTeasdale <85631937+JordanTeasdale@users.noreply.github.com>
2022-08-08 08:37:20 -0400

    Created GameManager script and object and ButtonFunctions script

---
+++ FieryToucansFPS/Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour {
+    public void Resume() {
+        if (GameManager.instance.isPaused) {
+            GameManager.instance.isPaused = false;
+            GameManager.instance.CursorUnlockUnpause();
+        }
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameManager.instance.CursorUnlockUnpause();
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
--- FieryToucansFPS/Assets/Scripts/GameManager.cs
+++ FieryToucansFPS/Assets/Scripts/GameManager.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+    public static GameManager instance;
+
+    public GameObject player;
+    //public PlayerController playerScript;
+
+    public GameObject pauseMenu;
+
+    public GameObject menuCurrentlyOpen;
+
+    public bool isPaused = false;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        //playerScript = player.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetButtonDown("Cancel")) {
+            isPaused = !isPaused;
+            menuCurrentlyOpen = pauseMenu;
+            menuCurrentlyOpen.SetActive(isPaused);
+
+            if (isPaused)
+                CursorLockPause();
+            else
+                CursorUnlockUnpause();
+        }
+    }
+
+    public void CursorLockPause() {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        Time.timeScale = 0;
+    }
+    public void CursorUnlockUnpause() {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+        menuCurrentlyOpen.SetActive(isPaused);
     }
 }
