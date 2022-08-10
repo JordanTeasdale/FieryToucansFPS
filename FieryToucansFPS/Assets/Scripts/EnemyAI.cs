a1e5291350eef45fb89bd684989d17cbd368d756
Briana Buttone <BKButtone@student.fullsail.edu>
2022-08-09 23:48:26 -0400

    more enemy AI added

--- FieryToucansFPS/Assets/Scripts/EnemyAI.cs
+++ FieryToucansFPS/Assets/Scripts/EnemyAI.cs
@@ -1,24 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAI : MonoBehaviour, IDamageable
 {
-    [SerializeField] int HP;
+    [Header("----- Components -----")]
+    [SerializeField] NavMeshAgent agent;
+
+    [Header("----- Enemy Stats -----")]
+    [Range(0, 10)] [SerializeField] int HP;
+    [Range(0, 10)] [SerializeField] int playerFaceSpeed;
+
+    [Header("----- Weapons Stats -----")]
+    [Range(0.1f, 5)] [SerializeField] float shootRate;
+    [Range(1, 10)] [SerializeField] int damage;
+    [Range(1, 10)] [SerializeField] int speed;
+    [Range(1, 10)] [SerializeField] int bulletDestroyTime;
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject bulletSpawnPos;
+
+    Vector3 playerDir;
+   
+
     // Start is called before the first frame update
-    void Start() {
-        
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
-    void Update() {
-        
+    void Update()
+    {
+        agent.SetDestination(GameManager.instance.player.transform.position);
+        playerDir = GameManager.instance.player.transform.position - transform.position;
+
+        facePlayer();
+
     }
 
-    public void takeDamage(int _damage) {
+    void facePlayer()
+    {
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            playerDir.y = 0;
+            Quaternion rotation = Quaternion.LookRotation(playerDir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * playerFaceSpeed);
+        }
+    }
+
+    public void TakeDamage(int _damage) {
         HP -= _damage;
         if (HP <= 0) {
             Destroy(gameObject);
         }
     }
+
+   
 }
