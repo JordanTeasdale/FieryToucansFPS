d3163db2c64e725d6cc41b197398d1b1d535a6d9
Logan-Krauss <qrio44@gmail.com>
2022-08-23 13:57:48 -0400

    Added Door Material & Updated Main Scene

--- FieryToucansFPS/Assets/Scripts/LevelSpawner.cs
+++ FieryToucansFPS/Assets/Scripts/LevelSpawner.cs
@@ -35,20 +35,20 @@ public class LevelSpawner : MonoBehaviour
             GameManager.instance.currentRoom = gameObject;
             for (int i = 0; i < doorLocations.Length; i++) {
                 if (doorFaceNorthSouth[i])
-                    GameObject.Instantiate(door, new Vector3(transform.position.x + doorLocations[i].x, 1.5f, transform.position.z + doorLocations[i].y), door.transform.rotation);
+                    GameObject.Instantiate(door, new Vector3(transform.position.x + doorLocations[i].x, door.transform.position.y, transform.position.z + doorLocations[i].y), door.transform.rotation);
                 else
-                    GameObject.Instantiate(door, new Vector3(transform.position.x + doorLocations[i].x, 1.5f, transform.position.z + doorLocations[i].y), Quaternion.Euler(new Vector3(0, 90, 0)));
+                    GameObject.Instantiate(door, new Vector3(transform.position.x + doorLocations[i].x, door.transform.position.y, transform.position.z + doorLocations[i].y), Quaternion.Euler(new Vector3(0, 90, 0)));
             }
             for (int i = 0; i < skeletonLocations.Length; i++) {
-                Instantiate(skeleton, new Vector3(transform.position.x + skeletonLocations[i].x, 1.5f, transform.position.z + skeletonLocations[i].y), skeleton.transform.rotation);
+                Instantiate(skeleton, new Vector3(transform.position.x + skeletonLocations[i].x, skeleton.transform.position.y, transform.position.z + skeletonLocations[i].y), skeleton.transform.rotation);
                 enemiesAlive++;
             }
             for (int i = 0; i < spiderLocations.Length; i++) {
-                Instantiate(spider, new Vector3(transform.position.x + spiderLocations[i].x, 1.5f, transform.position.z + spiderLocations[i].y), spider.transform.rotation);
+                Instantiate(spider, new Vector3(transform.position.x + spiderLocations[i].x, spider.transform.position.y, transform.position.z + spiderLocations[i].y), spider.transform.rotation);
                 enemiesAlive++;
             }
             for (int i = 0; i < dragonBossLocations.Length; i++) {
-                Instantiate(dragonBoss, new Vector3(transform.position.x + dragonBossLocations[i].x, 1.5f, transform.position.z + dragonBossLocations[i].y), dragonBoss.transform.rotation);
+                Instantiate(dragonBoss, new Vector3(transform.position.x + dragonBossLocations[i].x, dragonBoss.transform.position.y, transform.position.z + dragonBossLocations[i].y), dragonBoss.transform.rotation);
                 enemiesAlive++;
             }
 
