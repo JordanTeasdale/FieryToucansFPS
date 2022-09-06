1d74e633e7e6e1af8f25c8e464961d5836a1f72e
Logan-Krauss <qrio44@gmail.com>
2022-09-05 22:52:04 -0400

    Added Audio to checkpoint (Not fully functional)

--- FieryToucansFPS/Assets/Scripts/Checkpoint.cs
+++ FieryToucansFPS/Assets/Scripts/Checkpoint.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+
+    [SerializeField] AudioSource TempAudio;
+    [SerializeField] AudioClip TempAudioC;
+
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
-            GameManager.instance.RespawnPos.transform.position = transform.position;
+            TempAudio.PlayOneShot(TempAudioC, 1);
+            GameManager.instance.RespawnPos.transform.position = transform.position;           
             Destroy(gameObject);
         }
     }
+
+    
 }
