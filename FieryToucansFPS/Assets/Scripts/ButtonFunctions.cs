ffb5527f4a348a69b8a025f4cec3bfb478b28671
Logan-Krauss <qrio44@gmail.com>
2022-09-05 21:23:25 -0400

    leveltransitions and Menu work

--- FieryToucansFPS/Assets/Scripts/ButtonFunctions.cs
+++ FieryToucansFPS/Assets/Scripts/ButtonFunctions.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
-public class ButtonFunctions : MonoBehaviour {
 
+public class ButtonFunctions : MonoBehaviour
+{
+    LevelLoader levelLoader;
     public GameObject menuFirstButton, OptionsFirstButton, OptionsClosedButton, pauseFirstButton;
 
 
@@ -18,36 +20,52 @@ public class ButtonFunctions : MonoBehaviour {
         //set a new menu choice
         EventSystem.current.SetSelectedGameObject(pauseFirstButton);
 
+        //Going through the Heirarchy to find levelloader and gets crossfade object to call coroutine
+        levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
+
     }
 
-    public void Resume() {
-        if (GameManager.instance.isPaused) {
+    public void Resume()
+    {
+        if (GameManager.instance.isPaused)
+        {
             GameManager.instance.isPaused = false;
             GameManager.instance.CursorUnlockUnpause();
         }
     }
 
-    public void Restart() {
+    public void Restart()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         GameManager.instance.CursorUnlockUnpause();
+        levelLoader.CoRoutRun();
     }
 
-    public void Respawn() {
+    public void Respawn()
+    {
         GameManager.instance.playerScript.ResetHP();
         GameManager.instance.playerScript.Respawn();
         GameManager.instance.isPaused = false;
         GameManager.instance.CursorUnlockUnpause();
+        levelLoader.CoRoutRun();
 
     }
 
-    public void Quit() {
+    public void Quit()
+    {
+        levelLoader.CoRoutRun();
         Application.Quit();
     }
 
     public void PlaysGame()
     {
+        levelLoader.CoRoutRun();
         SceneManager.LoadScene("Level 1");
     }
 
-
+    public void QCreditScene()
+    {
+        levelLoader.CoRoutRun();
+        SceneManager.LoadScene("Credits");
+    }
 }
