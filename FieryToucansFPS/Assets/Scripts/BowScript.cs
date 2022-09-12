3cc489f380da7401ee1f8e2218c47f53e64c9582
JordanTeasdale <85631937+JordanTeasdale@users.noreply.github.com>
2022-09-12 15:17:49 -0400

    Shotgun, Bow, HandCannon, and Rile Models added

--- FieryToucansFPS/Assets/Scripts/BowScript.cs
+++ FieryToucansFPS/Assets/Scripts/BowScript.cs
@@ -15,8 +15,8 @@ public class BowScript : WeaponBase {
             yield return null;
         }
         Debug.Log(power);
-        GameObject bulletClone = Instantiate(bulletPrimary, GameManager.instance.gunPosition.transform.position, GameManager.instance.player.transform.rotation);
-        bulletClone.GetComponent<Rigidbody>().velocity = (GameManager.instance.player.transform.forward).normalized * (shootSpeedPrimary * (power / 250));
+        GameObject bulletClone = Instantiate(bulletPrimary, GameManager.instance.gunPosition.transform.position, GameManager.instance.playerScript.cameraMain.transform.rotation);
+        bulletClone.GetComponent<Rigidbody>().velocity = (GameManager.instance.playerScript.cameraMain.transform.forward).normalized * (shootSpeedPrimary * (power / 250));
         bulletPrimary.GetComponent<Bullet>().damage = (int)(bulletPrimary.GetComponent<Bullet>().damage * (power / 250));
         yield return new WaitForSeconds(shootRatePrimary);
         power = 5;
@@ -27,8 +27,8 @@ public class BowScript : WeaponBase {
         currentAmmo -= 3;
         GameManager.instance.playerScript.isShooting = true;
         for (int i = 0; i < 3; i++) {
-            GameObject bulletClone = Instantiate(bulletSecondary, GameManager.instance.gunPosition.transform.position, GameManager.instance.player.transform.rotation);
-            bulletClone.GetComponent<Rigidbody>().velocity = (GameManager.instance.player.transform.forward).normalized * shootSpeedSecondary; 
+            GameObject bulletClone = Instantiate(bulletSecondary, GameManager.instance.gunPosition.transform.position, GameManager.instance.playerScript.cameraMain.transform.rotation);
+            bulletClone.GetComponent<Rigidbody>().velocity = (GameManager.instance.playerScript.cameraMain.transform.forward).normalized * shootSpeedSecondary; 
             yield return new WaitForSeconds(0.5f);
         }
         yield return new WaitForSeconds(shootRateSecondary);
