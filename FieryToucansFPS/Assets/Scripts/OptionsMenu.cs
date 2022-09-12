0df2412128195047187077c5d70ab51e95619098
Jeremy Torres <torres.jeremy1029@gmail.com>
2022-09-12 14:38:15 -0400

    Options Menu Save Functionality
    Still finding a single location to load the data.

--- FieryToucansFPS/Assets/Scripts/OptionsMenu.cs
+++ FieryToucansFPS/Assets/Scripts/OptionsMenu.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+
 
 public class OptionsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] AudioMixer mainMixer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetFOV(float _FOV) {
+
+        GameManager.instance.playerScript.cameraMain.GetComponent<Camera>().fieldOfView = _FOV;
+        PlayerPrefs.SetFloat("FOV", _FOV);
+    }
+    public void SetVolMaster(float _volume) {
+
+        mainMixer.SetFloat("MasterVol", _volume);
+        PlayerPrefs.SetFloat("MasterVol", _volume);
+    }
+    public void SetVolMusic(float _volume) {
+
+        mainMixer.SetFloat("MusicVol", _volume);
+        PlayerPrefs.SetFloat("MusicVol", _volume);
+    }
+    public void SetVolSFX(float _volume) {
+
+        mainMixer.SetFloat("SFXVol", _volume);
+        PlayerPrefs.SetFloat("SFXVol", _volume);
     }
 }
