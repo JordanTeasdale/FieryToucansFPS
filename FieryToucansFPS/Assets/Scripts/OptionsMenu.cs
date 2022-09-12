efa0e96a8f6b0310f6be6b462dcae7ec38b10f7e
Jeremy Torres <torres.jeremy1029@gmail.com>
2022-09-12 15:31:00 -0400

    OptionsMenu Update

--- FieryToucansFPS/Assets/Scripts/OptionsMenu.cs
+++ FieryToucansFPS/Assets/Scripts/OptionsMenu.cs
@@ -8,24 +8,36 @@ public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] AudioMixer mainMixer;
 
+    float FOV;
+    float masterVol;
+    float musicVol;
+    float SFXVol;
+
     public void SetFOV(float _FOV) {
 
-        GameManager.instance.playerScript.cameraMain.GetComponent<Camera>().fieldOfView = _FOV;
-        PlayerPrefs.SetFloat("FOV", _FOV);
+        FOV = _FOV;
+        GameManager.instance.playerScript.cameraMain.GetComponent<Camera>().fieldOfView = FOV;
+       
     }
     public void SetVolMaster(float _volume) {
-
+        masterVol = _volume;
         mainMixer.SetFloat("MasterVol", _volume);
-        PlayerPrefs.SetFloat("MasterVol", _volume);
+        
     }
     public void SetVolMusic(float _volume) {
-
+        musicVol = _volume;
         mainMixer.SetFloat("MusicVol", _volume);
-        PlayerPrefs.SetFloat("MusicVol", _volume);
+        
     }
     public void SetVolSFX(float _volume) {
-
+        SFXVol = _volume;
         mainMixer.SetFloat("SFXVol", _volume);
-        PlayerPrefs.SetFloat("SFXVol", _volume);
+       
+    }
+    public void Save() {
+        PlayerPrefs.SetFloat("FOV", FOV);
+        PlayerPrefs.SetFloat("MasterVol", masterVol);
+        PlayerPrefs.SetFloat("MusicVol", musicVol);
+        PlayerPrefs.SetFloat("SFXVol", SFXVol);
     }
 }
