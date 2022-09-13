230efcc714cac09d65723c7eca51f7cec33019eb
JordanTeasdale <85631937+JordanTeasdale@users.noreply.github.com>
2022-09-13 19:54:14 -0400

    Added reticle functionality
    just need the actual reticles

---
+++ FieryToucansFPS/Assets/CreditsScroll.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsScroll : MonoBehaviour
+{
+    [SerializeField] float timer;
+    LevelLoader levelLoader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.localPosition = Vector3.zero;
+        levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        gameObject.transform.position += new Vector3(0, 0.5f, 0);
+        if (timer <= 0) {
+            levelLoader.CoRoutRun();
+            SceneManager.LoadScene("Main Menu");
+        }
+
+    }
+}
