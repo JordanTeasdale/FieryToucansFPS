367f7aad9d782bade58cba4be54eee43ba413d57
Jeremy Torres <torres.jeremy1029@gmail.com>
2022-09-13 15:53:17 -0400

    Created Audio Manager
    Sounds need to be set up for the game.

---
+++ FieryToucansFPS/Assets/Scripts/Sound.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+
+    public string name;
+   
+    public AudioClip orignalFile;
+    public AudioMixer mixer;
+    public AudioMixerGroup mixerGroup;
+
+    [Range(-1f, 1f)] public float pitch;
+    [Range(0, 1f)] public float Volume;
+
+    public bool isLooping;
+
+
+   [HideInInspector] public AudioSource sourceOfSound;
+
+
+   
+    
+}
