bccb2700943a2d244cf80fc814ef77161ee29b49
Jeremy Torres <torres.jeremy1029@gmail.com>
2022-09-18 18:46:17 -0400

    UI fixes and Background Music Fixes

--- FieryToucansFPS/Assets/Scripts/Level3Music.cs
+++ FieryToucansFPS/Assets/Scripts/Level3Music.cs
@@ -9,6 +9,7 @@ public class Level3Music : MonoBehaviour
     void Update()
     {
         if (FindObjectOfType<AudioManager>().percentOfListComplete == 1f && isPlaying == false) {
+            Debug.Log("entered");
             FindObjectOfType<AudioManager>().Play("In The Abyss");
             isPlaying = true;
         }
