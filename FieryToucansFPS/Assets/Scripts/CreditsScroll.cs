69d6ba164f5d90d0865b7d69c3e1bfb149a9f5c7
JordanTeasdale <85631937+JordanTeasdale@users.noreply.github.com>
2022-09-18 13:01:26 -0400

    Added textures to main menu and credits scenes and adjusted credits size and scroll speed to feel better

--- FieryToucansFPS/Assets/Scripts/CreditsScroll.cs
+++ FieryToucansFPS/Assets/Scripts/CreditsScroll.cs
@@ -3,22 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CreditsScroll : MonoBehaviour
-{
+public class CreditsScroll : MonoBehaviour {
     [SerializeField] float timer;
     LevelLoader levelLoader;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         gameObject.transform.localPosition = Vector3.zero;
         levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate() {
         timer -= Time.deltaTime;
-        gameObject.transform.position += new Vector3(0, 0.75f, 0);
+        gameObject.transform.position += new Vector3(0, 5, 0);
         if (timer <= 0) {
             levelLoader.CoRoutRun();
             SceneManager.LoadScene("Main Menu");
